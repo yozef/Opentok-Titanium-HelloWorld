846d34bb0bc29c4e0d6866e1beb3ffc9dcda1b7c
Ankur Oberoi <aoberoi@gmail.com>
2014-02-07 17:15:33 -0500

    adds missing semicolons in config

--- Resources/config.js
+++ Resources/config.js
@@ -3,8 +3,8 @@ var CONFIG  = {
 	token     : '', // Add your generated token here. Generate on at https://dashboard.tokbox.com/projects
 	sessionId : '', // Add your generated Session ID here. Generated from above.
 	baseUrl   : 'http://opentok.github.com/opentok-titanium-mobile/simulator-test.html'
-}
+};
 
 CONFIG.webUrl = CONFIG.baseUrl + '#apiKey=' + encodeURIComponent(CONFIG.apiKey) + '&token=' + encodeURIComponent(CONFIG.token) + '&sessionId=' + encodeURIComponent(CONFIG.sessionId);
 
-module.exports = CONFIG
+module.exports = CONFIG;
